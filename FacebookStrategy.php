d4a08fd0a7b47f374e0b48b2a531779774feb343
Petro <info@xyz.net.ua>
2017-05-22 10:32:32 +0300

    supporting grap v2.8

--- FacebookStrategy.php
+++ FacebookStrategy.php
@@ -13,132 +13,137 @@
 
 class FacebookStrategy extends OpauthStrategy{
 
-	/**
-	 * Compulsory config keys, listed as unassociative arrays
-	 * eg. array('app_id', 'app_secret');
-	 */
-	public $expects = array('app_id', 'app_secret');
+    /**
+     * Compulsory config keys, listed as unassociative arrays
+     * eg. array('app_id', 'app_secret');
+     */
+    public $expects = array('app_id', 'app_secret');
 
-	/**
-	 * Optional config keys with respective default values, listed as associative arrays
-	 * eg. array('scope' => 'email');
-	 */
-	public $defaults = array(
-		'redirect_uri' => '{complete_url_to_strategy}int_callback'
-	);
+    /**
+     * Optional config keys with respective default values, listed as associative arrays
+     * eg. array('scope' => 'email');
+     */
+    public $defaults = array(
+        'redirect_uri' => '{complete_url_to_strategy}int_callback'
+    );
 
-	/**
-	 * Auth request
-	 */
-	public function request(){
-		$url = 'https://www.facebook.com/dialog/oauth';
-		$params = array(
-			'client_id' => $this->strategy['app_id'],
-			'redirect_uri' => $this->strategy['redirect_uri']
-		);
+    /**
+     * Auth request
+     */
+    public function request(){
+        $url = 'https://www.facebook.com/v2.8/dialog/oauth';
+        $params = array(
+            'client_id' => $this->strategy['app_id'],
+            'redirect_uri' => $this->strategy['redirect_uri']
+        );
 
-		if (!empty($this->strategy['scope'])) $params['scope'] = $this->strategy['scope'];
-		if (!empty($this->strategy['state'])) $params['state'] = $this->strategy['state'];
-		if (!empty($this->strategy['response_type'])) $params['response_type'] = $this->strategy['response_type'];
-		if (!empty($this->strategy['display'])) $params['display'] = $this->strategy['display'];
-		if (!empty($this->strategy['auth_type'])) $params['auth_type'] = $this->strategy['auth_type'];
+        if (!empty($this->strategy['scope'])) $params['scope'] = $this->strategy['scope'];
+        if (!empty($this->strategy['state'])) $params['state'] = $this->strategy['state'];
+        if (!empty($this->strategy['response_type'])) $params['response_type'] = $this->strategy['response_type'];
+        if (!empty($this->strategy['display'])) $params['display'] = $this->strategy['display'];
+        if (!empty($this->strategy['auth_type'])) $params['auth_type'] = $this->strategy['auth_type'];
 
-		$this->clientGet($url, $params);
-	}
+        $this->clientGet($url, $params);
+    }
 
-	/**
-	 * Internal callback, after Facebook's OAuth
-	 */
-	public function int_callback(){
-		if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
-			$url = 'https://graph.facebook.com/oauth/access_token';
-			$params = array(
-				'client_id' =>$this->strategy['app_id'],
-				'client_secret' => $this->strategy['app_secret'],
-				'redirect_uri'=> $this->strategy['redirect_uri'],
-				'code' => trim($_GET['code'])
-			);
-			$response = $this->serverGet($url, $params, null, $headers);
+    /**
+     * Internal callback, after Facebook's OAuth
+     */
+    public function int_callback(){
+        if (array_key_exists('code', $_GET) && !empty($_GET['code'])){
+            $url = 'https://graph.facebook.com/oauth/access_token';
+            $params = array(
+                'client_id' =>$this->strategy['app_id'],
+                'client_secret' => $this->strategy['app_secret'],
+                'redirect_uri'=> $this->strategy['redirect_uri'],
+                'code' => trim($_GET['code'])
+            );
+            $response = $this->serverGet($url, $params, null, $headers);
+            $results = json_decode($response);
 
-            $results = json_decode($response, true);
+            if (!empty($results) && !empty($results->access_token)){
+                $me = $this->me($results->access_token);
+                $this->auth = array(
+                    'provider' => 'Facebook',
+                    'uid' => $me->id,
+                    'info' => array(
+                        'name' => $me->name,
+                        'image' => 'https://graph.facebook.com/v2.8/'.$me->id.'/picture?type=large'
+                    ),
+                    'credentials' => array(
+                        'token' => $results->access_token,
+                        'expires' => date('c', time() + $results->expires_in)
+                    ),
+                    'raw' => $me
+                );
 
-			if (!empty($results) && !empty($results['access_token'])){
-				$me = $this->me($results['access_token']);
+                if (!empty($me->email)) $this->auth['info']['email'] = $me->email;
+                if (!empty($me->name)) $this->auth['info']['nickname'] = $me->name;
+                if (!empty($me->first_name)) $this->auth['info']['first_name'] = $me->first_name;
+                if (!empty($me->last_name)) $this->auth['info']['last_name'] = $me->last_name;
+                if (!empty($me->link)) $this->auth['info']['urls']['facebook'] = $me->link;
 
-				$this->auth = array(
-					'provider' => 'Facebook',
-					'uid' => $me->id,
-					'info' => array(
-						'name' => $me->name,
-						'image' => 'https://graph.facebook.com/v2.6/'.$me->id.'/picture?type=large'
-					),
-					'credentials' => array(
-						'token' => $results['access_token'],
-						'expires' => date('c', time() + $results['expires'])
-					),
-					'raw' => $me
-				);
+                /**
+                 * Missing optional info values
+                 * - description
+                 * - phone: not accessible via Facebook Graph API
+                 */
 
-				if (!empty($me->email)) $this->auth['info']['email'] = $me->email;
-				if (!empty($me->name)) $this->auth['info']['nickname'] = $me->name;
-				if (!empty($me->first_name)) $this->auth['info']['first_name'] = $me->first_name;
-				if (!empty($me->last_name)) $this->auth['info']['last_name'] = $me->last_name;
-				if (!empty($me->link)) $this->auth['info']['urls']['facebook'] = $me->link;
+                $this->callback();
+            }
+            else{
+                $error = array(
+                    'provider' => 'Facebook',
+                    'code' => 'access_token_error',
+                    'message' => 'Failed when attempting to obtain access token',
+                    'raw' => $headers
+                );
 
-				/**
-				 * Missing optional info values
-				 * - description
-				 * - phone: not accessible via Facebook Graph API
-				 */
+                $this->errorCallback($error);
+            }
+        }
+        else{
+            $error = array(
+                'provider' => 'Facebook',
+                'code' => $_GET['error_code'],
+                'message' => $_GET['error_message'],
+                'raw' => $_GET
+            );
 
-				$this->callback();
-			}
-			else{
-				$error = array(
-					'provider' => 'Facebook',
-					'code' => 'access_token_error',
-					'message' => 'Failed when attempting to obtain access token',
-					'raw' => $headers
-				);
+            $this->errorCallback($error);
+        }
+    }
 
-				$this->errorCallback($error);
-			}
-		}
-		else{
-			$error = array(
-				'provider' => 'Facebook',
-				'code' => $_GET['error'],
-				'message' => $_GET['error_description'],
-				'raw' => $_GET
-			);
+    /**
+     * Queries Facebook Graph API for user info
+     *
+     * @param string $access_token
+     * @return array Parsed JSON results
+     */
+    private function me($access_token){
 
-			$this->errorCallback($error);
-		}
-	}
+        $fields = 'id,name,email';//default value
+        if ( isset($this->strategy['fields']) ) {
+            $fields = $this->strategy['fields'];
+        }
 
-	/**
-	 * Queries Facebook Graph API for user info
-	 *
-	 * @param string $access_token
-	 * @return array Parsed JSON results
-	 */
-	private function me($access_token){
-		$me = $this->serverGet('https://graph.facebook.com/v2.3/me', array('access_token' => $access_token), null, $headers);
-		if (!empty($me)){
-			return json_decode($me);
-		}
-		else{
-			$error = array(
-				'provider' => 'Facebook',
-				'code' => 'me_error',
-				'message' => 'Failed when attempting to query for user information',
-				'raw' => array(
-					'response' => $me,
-					'headers' => $headers
-				)
-			);
+        $me = $this->serverGet('https://graph.facebook.com/v2.8/me', array('access_token' => $access_token, 'fields' => $fields), null, $headers);
 
-			$this->errorCallback($error);
-		}
-	}
+        if (!empty($me)){
+            return json_decode($me);
+        }
+        else{
+            $error = array(
+                'provider' => 'Facebook',
+                'code' => 'me_error',
+                'message' => 'Failed when attempting to query for user information',
+                'raw' => array(
+                    'response' => $me,
+                    'headers' => $headers
+                )
+            );
+
+            $this->errorCallback($error);
+        }
+    }
 }
